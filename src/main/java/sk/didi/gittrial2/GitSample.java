868601d118ef75076879f0f23f320018c07c83c2
Daniela Donocikova <daniela.donocikova@rohlik.cz>
2018-11-01 12:25:49 +0100

    initial project commit

---
+++ src/main/java/sk/didi/gittrial2/GitSample.java
@@ -0,0 +1,7 @@
+package sk.didi.gittrial2;
+
+import lombok.Data;
+
+@Data
+public class GitSample {
+}
---
+++ src/main/java/sk/didi/gittrial2/Gittrial2Application.java
@@ -0,0 +1,13 @@
+package sk.didi.gittrial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittrial2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gittrial2Application.class, args);
+        System.out.println("I am Git Sample");
+    }
+}
